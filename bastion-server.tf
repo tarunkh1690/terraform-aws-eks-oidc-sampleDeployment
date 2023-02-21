ad96b666ce3a1d1350bb64e383197b847df5dec4
Tarun Khare <tarunkh1690@gmail.com>
2023-02-22 01:37:13 +0530

    start bastion

--- bastion-server.tf
+++ bastion-server.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bastion" {
-  count                  = 0
+  count                  = 1
   ami                    = "ami-074dc0a6f6c764218"
   instance_type          = "t2.micro"
   key_name               = "k8s-cluster-key"
