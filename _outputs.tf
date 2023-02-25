356779d78c0c9d606aec416432aea3194888fabc
Tarun Khare <tarunkh1690@gmail.com>
2023-02-25 12:26:06 +0530

    removed oidc

--- _outputs.tf
+++ _outputs.tf
@@ -22,6 +22,6 @@ output "test_policy_arn" {
   value = aws_iam_role.test_oidc.arn
 }
 
-output "eks_cluster_autoscaler_arn" {
-  value = aws_iam_role.eks_cluster_autoscaler.arn
-}
+#output "eks_cluster_autoscaler_arn" {
+#  value = aws_iam_role.eks_cluster_autoscaler.arn
+#}
